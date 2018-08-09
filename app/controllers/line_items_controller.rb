e9329a1f24e593c69dcad3f0a23f09259a90671e
ICanDoAllThingThroughChrist <ctlee9096@gmail.com>
2018-08-09 08:52:00 -0500

    created an Ajax-Based Cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -31,9 +31,10 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart,
-          notice: 'Line Item was successfully created.'}
-        format.json { render :show, status: :created, location: @line_item }
+        format.html { redirect_to store_index_url }
+        format.js
+        format.json { render :show,
+           status: :created, location: @line_item }
       else
         format.html { render :new }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
